9a074a8b4494dd91baad3b00c3a2365437a1a62f
prasanthxpoffice <prasanthxpoffice@gmail.com>
2025-11-14 01:28:41 +0400

    Db

---
+++ db/Insert data.sql
@@ -0,0 +1,82 @@
+USE [IAS]
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 1, N'Person 1', N'#008080', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 2, N'Person 2', N'#FFA500', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 3, N'Person 3', N'#008080', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 4, N'Person 4', N'#FFA500', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 5, N'Person 5', N'#00FFFF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 1, N'Person 1', N'#008080', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 6, N'Person 6', N'#FFA500', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', N'Person', N'Character', CAST(N'1900-01-01' AS Date), 3, N'Person 3', N'#008080', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, N'Vehicle', N'Car', CAST(N'1900-01-01' AS Date), 11, N'Rav 4', N'#FFFF00', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, N'Vehicle', N'Car', CAST(N'1900-01-01' AS Date), 12, N'Yaris', N'#FFFF00', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, N'Vehicle', N'Car', CAST(N'1900-01-01' AS Date), 13, N'BYD', N'#FFFF00', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL)
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 1, N'Incident 1', N'#0000FF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, N'Weapon', N'Weapon', CAST(N'1900-01-01' AS Date), 1, N'Weapon 1', N'#00FFFF')
+GO
+INSERT [graphdb].[IncidentsData] ([NT_A_NT], [ED_A_ED], [DATE_A_DATE], [ID_A_ID], [TEXT_A_TEXT], [COLOR_A_COLOR], [NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_C_NT], [ED_C_ED], [DATE_C_DATE], [ID_C_ID], [TEXT_C_TEXT], [COLOR_C_COLOR], [NT_D_NT], [ED_D_ED], [DATE_D_DATE], [ID_D_ID], [TEXT_D_TEXT], [COLOR_D_COLOR]) VALUES (N'Incident', N'Incident', CAST(N'1900-01-01' AS Date), 2, N'Incident 2', N'#0000FF', NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, NULL, NULL, CAST(N'1900-01-01' AS Date), NULL, NULL, NULL, N'Weapon', N'Weapon', CAST(N'1900-01-01' AS Date), 2, N'Weapon 2', N'#00FFFF')
+GO
+INSERT [graphdb].[Nodes] ([Column_ID], [ColumnEn], [ColumnAr], [IsActive], [ColumnColor]) VALUES (N'ID_A_ID', N'Incident
+ En', N'Incident
+ Ar', 1, N'#0000FF')
+GO
+INSERT [graphdb].[Nodes] ([Column_ID], [ColumnEn], [ColumnAr], [IsActive], [ColumnColor]) VALUES (N'ID_B_ID', N'Person
+
+ En', N'Person
+
+ Ar', 1, N'#008080')
+GO
+INSERT [graphdb].[Nodes] ([Column_ID], [ColumnEn], [ColumnAr], [IsActive], [ColumnColor]) VALUES (N'ID_C_ID', N'Vehicle
+
+ En', N'Vehicle
+
+ Ar', 1, N'#FFFF00')
+GO
+INSERT [graphdb].[Nodes] ([Column_ID], [ColumnEn], [ColumnAr], [IsActive], [ColumnColor]) VALUES (N'ID_D_ID', N'Weapon
+
+ En', N'Weapon
+
+ Ar', 1, N'#00FFFF')
+GO
+INSERT [graphdb].[Nodes] ([Column_ID], [ColumnEn], [ColumnAr], [IsActive], [ColumnColor]) VALUES (N'ID_R_ID', N'Relation
+
+ En', N'Relation
+
+ Ar', 1, N'#FF00FF')
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_A_ID', N'ID_A_ID', N'ID_B_ID', N'->        ', NULL, N'Person Involved', N'Person Involved')
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_A_ID', N'ID_A_ID', N'ID_C_ID', N'->        ', NULL, N'Vehicle Used', N'Vehicle Used')
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_A_ID', N'ID_A_ID', N'ID_D_ID', N'->        ', NULL, N'Weapon Used', N'Weapon Used')
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_B_ID', N'ID_B_ID', N'ID_A_ID', N'->        ', NULL, N'Incident', N'Incident')
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_B_ID', N'ID_B_ID', N'ID_R_ID', N'->        ', N'ID_R_ID', NULL, NULL)
+GO
+INSERT [graphdb].[Relations] ([Source_Column_ID], [Search_Column_ID], [Display_Column_ID], [Direction], [Relation_Column_ID], [RelationEn], [RelationAr]) VALUES (N'ID_R_ID', N'ID_B_ID', N'ID_A_ID', N'->        ', NULL, N'Incident', N'Incident')
+GO
+INSERT [graphdb].[RelationsData] ([NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_R_NT], [ED_R_ED], [DATE_R_DATE], [ID_R_ID], [TEXT_R_TEXT], [COLOR_R_COLOR]) VALUES (N'Person', N'Character', CAST(N'1900-01-01' AS Date), 1, N'Person 1', N'#008080', N'Relation', N'Brother', CAST(N'1900-01-01' AS Date), 2, N'Person 2', N'#008080')
+GO
+INSERT [graphdb].[RelationsData] ([NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_R_NT], [ED_R_ED], [DATE_R_DATE], [ID_R_ID], [TEXT_R_TEXT], [COLOR_R_COLOR]) VALUES (N'Person', N'Character', CAST(N'1900-01-01' AS Date), 1, N'Person 1', N'#008080', N'Relation', N'Sister', CAST(N'1900-01-01' AS Date), 3, N'Person 3', N'#008080')
+GO
+INSERT [graphdb].[RelationsData] ([NT_B_NT], [ED_B_ED], [DATE_B_DATE], [ID_B_ID], [TEXT_B_TEXT], [COLOR_B_COLOR], [NT_R_NT], [ED_R_ED], [DATE_R_DATE], [ID_R_ID], [TEXT_R_TEXT], [COLOR_R_COLOR]) VALUES (N'Person', N'Character', CAST(N'1900-01-01' AS Date), 4, N'Person 4', N'#008080', N'Relation', N'Father', CAST(N'1900-01-01' AS Date), 5, N'Person 5', N'#008080')
+GO
+SET IDENTITY_INSERT [graphdb].[ViewRegistry] ON 
+GO
+INSERT [graphdb].[ViewRegistry] ([ViewID], [ViewDescriptionEn], [ViewDescriptionAr], [ViewNameEn], [ViewNameAr], [ViewDB], [ViewSchema]) VALUES (1, N'Incidents En', N'Incidents Ar', N'IncidentsData', N'IncidentsData', N'IAS', N'graphdb')
+GO
+INSERT [graphdb].[ViewRegistry] ([ViewID], [ViewDescriptionEn], [ViewDescriptionAr], [ViewNameEn], [ViewNameAr], [ViewDB], [ViewSchema]) VALUES (2, N'Relations En', N'Relations Ar', N'RelationsData', N'RelationsData', N'IAS', N'graphdb')
+GO
+SET IDENTITY_INSERT [graphdb].[ViewRegistry] OFF
+GO
